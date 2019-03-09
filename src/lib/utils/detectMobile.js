0e47d232872de1cc005be9a9949b0f3f8858d866
Kai Hotz <kaihotz@me.com>
2019-03-09 17:54:24 +0100

    isMobile adjusted

--- src/lib/utils/detectMobile.js
+++ src/lib/utils/detectMobile.js
@@ -1,9 +1,11 @@
+const windowsObj = typeof window !== 'undefined'
+
 const detectMobile = {
-  Android: () => navigator.userAgent.match(/Android/i),
-  BlackBerry: () => navigator.userAgent.match(/BlackBerry/i),
-  iOS: () => navigator.userAgent.match(/iPhone|iPad|iPod/i),
-  Opera: () => navigator.userAgent.match(/Opera Mini/i),
-  Windows: () => navigator.userAgent.match(/IEMobile/i),
+  Android: () => windowsObj && navigator.userAgent.match(/Android/i),
+  BlackBerry: () => windowsObj && navigator.userAgent.match(/BlackBerry/i),
+  iOS: () => windowsObj && navigator.userAgent.match(/iPhone|iPad|iPod/i),
+  Opera: () => windowsObj && navigator.userAgent.match(/Opera Mini/i),
+  Windows: () => windowsObj && navigator.userAgent.match(/IEMobile/i),
   any: () => (detectMobile.Android() || detectMobile.BlackBerry() || detectMobile.iOS() || detectMobile.Opera() || detectMobile.Windows()),
 }
 
