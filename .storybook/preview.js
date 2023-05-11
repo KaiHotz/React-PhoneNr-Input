630447cda88715ce89e709e47d208b279e7c6f70
KaiHotz <kaihotz@me.com>
2023-05-11 19:37:03 +0400

    dependencies and storybook update

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,11 @@
-import './storybook.css';
-import './main.scss';
+import './preview.scss';
 
-import { addDecorator } from '@storybook/react';
-import { withInfo } from '@storybook/addon-info';
-import { withPropsTable } from 'storybook-addon-react-docgen';
-
-addDecorator(withInfo);
-addDecorator(withPropsTable({
-  propTablesExclude: ['Story']
-}));
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
