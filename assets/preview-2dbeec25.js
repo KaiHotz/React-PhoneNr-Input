01dfe09d95e98669250495accdc7986e2ca6b7a5
KaiHotz <kaihotz@me.com>
2023-08-26 22:33:20 +0400

    Updates

---
+++ assets/preview-2dbeec25.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./iframe-b7d6705b.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-3PUGWF3O-2492f9fc.js").then(e=>e.D),["./DocsRenderer-3PUGWF3O-2492f9fc.js","./iframe-b7d6705b.js","./index-61bf1805.js","./_commonjsHelpers-de833af9.js","./_getPrototype-74a4a53f.js","./index-d475d2ea.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
+//# sourceMappingURL=preview-2dbeec25.js.map
